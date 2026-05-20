fe130f3b80cbe50550ee4e9f2b3a52d039aa9f8d
kynssss <joaquingabriel0209@gmail.com>
2026-05-20 11:32:14 -0700

    Add files via upload

--- HTMLManager.java
+++ HTMLManager.java
@@ -3,4 +3,14 @@ import java.util.*;
 public class HTMLManager {
   private Queue<HTMLTag> tags;
   
+  public HTMLManager(Queue<HTMLTag> html){
+     if(html == null) {
+        throw new IllegalArgumentException("nothing");
+     } else {
+        while(!html.isEmpty()){
+           tags.add(html.remove());
+        }
+     }
+  }
+  
 }
